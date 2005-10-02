e28578ebdcac8d8b477407b918ae9ec2d91bb7b0
Nathan Bird <birdman@acceleration.net>
2005-10-02 18:52:46 -0400

    Creature Interface Specification v0.1
    darcs-hash:20051002225246-ed59e-9d81a3d7b48319d570f0a7eff6b56f606d0efcab.gz

--- src/Creature.lisp
+++ src/Creature.lisp
@@ -3,3 +3,27 @@
 (defparameter +possible-creatures+ '())
 
 (defclass creature () '())
+
+
+
+;;;;Functions available for a creature
+
+;;;; Define-creature-method is a macro to create a method
+;;;; specialized on a type of creature
+(defmacro define-creature-op (name lambda-list &body body)
+  "Ease in the creation of operations a creature can perform.
+	For now I'm thinking this will mean having it specialized on a creature.")
+
+
+(define-creature-op look ((direction nil))
+  "Examine the location the creature is currently standing at if the location
+is nill, or look in the specified direction.")
+
+(define-creature-op move (direction)
+  "Move the creature in a specified direction.")
+
+(define-creature-op feed ()
+  "Feed from the energy source at the current location.")
+
+(define-creature-op reproduce ()
+  "Creature reproduction.")
