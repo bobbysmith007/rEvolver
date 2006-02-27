90d93ac7bab213f825b7e83b3e7703253620867c
russ <russ@acceleration.net>
2006-02-26 20:49:04 -0500

    working on DNA.lisp
    darcs-hash:20060227014904-03e4f-eddfa3270f8199fb3a06a213e27da61bc4a7414a.gz

--- src/dna.lisp
+++ src/dna.lisp
@@ -25,6 +25,49 @@
 ; (energyp &optional <node>) ;;energy at optional node or current node 
 ; (feed) ;; get energy from right here
 
-(defparameter Creature-DNA '(
-			     
-			     ))
+;;; A grammer specification for the DNA language
+;;; Expression : (alist of Constrainer notes)  are constrainer 
+(defparameter Creature-DNA-1
+  '(
+    (?Expression -> (
+		    (lambda ?Symbol ?Expression => lambda)
+		    (gamma ?Expression : ((type . lambda))
+			   ?Expression => gamma)
+
+		    (cons ?Expression ?Expression => cons)
+		    (cdr ?Expression : ((type . 'list)) => cdr)
+		    (car ?Expression : ((type . 'list)) => car)
+
+		    (tag ?Expression : ((doc . "Connects a type to a value to a val"))
+			 ?Expression => tag)
+		    (type ?Expression => type)
+		    (value ?Expression => value)
+		     
+		    (or ?Expression ?Expression => or)
+		    (if ?Expression ?Expression ?Expression => if)
+		    (not ?Expression => not)
+		    ;predicates
+		    (equal? ?Expression ?Expression => equal)
+
+		    (move ?Expression : ((type .  (equal? null ?value))) => move)
+		    (energy? ?Expression : ((type . node)) => energy?)
+		    (nil => 'nil)
+		    (?Type ) 
+		    )
+     
+     )
+    (?Type -> ((node => node)
+	      (function => function)
+	      (list => list)
+	      (atom => atom)
+	      (number => number)
+		
+	      )
+     )
+    (?Symbol -> ((gensym => 'gensym)
+		 (*gened-sym* => '*gened-sym*)))
+    ))
+
+(defun process-grammer-definition (grammer)
+  
+  )
