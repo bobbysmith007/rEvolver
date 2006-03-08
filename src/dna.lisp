90e1e8c5c85a1682a3bc1f26d7b86aec5c8c06a0
russ <russ@acceleration.net>
2006-03-08 01:41:10 -0500

    generating standardized trees with symbols
    darcs-hash:20060308064110-03e4f-77b2b6419f39390f65ff49da1db8d77cbe09800f.gz

--- src/dna.lisp
+++ src/dna.lisp
@@ -5,7 +5,7 @@
 ;;(?RightPart -> name ?rewrite-name ?rewrite-name => (write-part))
 ;;(?rewrite-name -> )
 
-(defparameter +depth-bound+ 10
+(defparameter +depth-bound+ 5
   "the depth at which tree generation terminates")
 
 (defun process-grammar-definition (grammar)
@@ -52,9 +52,7 @@ TODO: This should probably actually make some sort of struct rather than redicul
 
 (defun write-part (rewrites-to)
   (let ((write-part (cadr rewrites-to)))
-    (if (listp write-part)
-	(car write-part)
-	write-part)))
+    write-part))
 
 (defun possibilities (processed-grammar rewrite-name) 
   (cdr (assoc rewrite-name processed-grammar)))
@@ -66,73 +64,130 @@ TODO: This should probably actually make some sort of struct rather than redicul
       val))
 
 (defun pick-possibility (list current-depth)
+  (declare (ignore current-depth))
   (when list
 	(random-elt list)))
 
-(defun get-child-nodes (processed-grammar rewrite-tokens current-depth)
+(defun get-child-nodes (processed-grammar rewrite-tokens current-depth symbol-table)
   (mapcar
    (lambda (new-expansion)
-     (generate-tree processed-grammar (rewrite-node-name new-expansion) (1- current-depth)))
+     (let ((new-sub-tree
+	    (generate-tree processed-grammar
+			   (rewrite-node-name new-expansion)
+			   (1- current-depth)
+			   symbol-table)))
+       
+       (cond ((eq new-sub-tree 'gensym)
+	      (car (push (gensym) symbol-table)))
+	     ((eq new-sub-tree '*gened-sym*)
+	      (or (random-elt symbol-table)
+		  (when (eq '?Symbol new-expansion)
+		      (gensym))))
+	     (T new-sub-tree))))
    rewrite-tokens))
 
+(defun depth-first-expression-replace (list symbol-list value-list)
+  "Replaces tokens from symbol-list with values from value-list in the list
+  (depth-first-expression-replace '(gamma (gamma cons ?expression) ?expression) '(?Expression ?Expression) '(a b))
+  => '(gamma (gamma cons a) b) "
+  (labels ((depth-first-expression-replace (list)
+	     (unless (null list)
+	       (let ((elem (car list))
+		     (rest (rest list)))
+		 (cond ((listp elem)
+			(cons (depth-first-expression-replace elem)
+			      (depth-first-expression-replace rest)))
+		       ((eq (car symbol-list) elem)
+			(pop symbol-list)
+			(cons (pop value-list)
+			      (depth-first-expression-replace rest)))
+		       (T
+			(cons elem
+			      (depth-first-expression-replace rest))))))))
+    
+    (depth-first-expression-replace list)))
+;(depth-first-expression-replace '(gamma (gamma cons ?expression) ?expression) '(?Expression ?Expression) '((a) (b)))
 
-(defun generate-tree (processed-grammar rewrite-name &optional (current-depth +depth-bound+))
+
+(defun generate-tree (processed-grammar rewrite-name &optional (current-depth +depth-bound+) (symbol-table nil))
   (declare (optimize (debug 3)))
   
     (let* (;get all of the rewrite possibilities of the rule
 	   (possibilities (if (= 0 current-depth)
 			      (possibilities processed-grammar '?Terminal)
-			      (possibilities processed-grammar rewrite-name)))	 
+			      (possibilities processed-grammar rewrite-name)))
 	   (chosen (pick-possibility possibilities current-depth))
 	   
 	   ;; Get all the expansions for this rewrite
 	   (rewrite-tokens (get-rewrite-tokens chosen))
+
 	   ;; what tree-node to build
 	   (write-part (write-part chosen))
-	   (write-name (rewrite-node-name write-part))
-	   
+	   	   
 	   ;; get all the subtrees neccessary for this
-	   (child-nodes (get-child-nodes processed-grammar rewrite-tokens current-depth)))
+	   (child-nodes (get-child-nodes processed-grammar rewrite-tokens current-depth symbol-table) )
+	   (write-tree (if (atom write-part) write-part
+			   (depth-first-expression-replace write-part rewrite-tokens child-nodes))))
       
-      (format T "~%------~%chosen:~a ~%" chosen)      
-      (format T "rewrite-tokens:~a ~%" rewrite-tokens)
-      (format T "write-part:~a ~%" write-part)
-      (format T "children:~a ~%" child-nodes)
-      (format T "current-depth:~a~%" current-depth)
+;      (format T "~%------~%chosen:~a ~%" chosen)      
+;      (format T "rewrite-tokens:~a ~%" rewrite-tokens)
+;      (format T "write-part:~a ~%" write-part)
+;      (format T "write-tree:~a ~%" write-tree)
+;      (format T "children:~a ~%" child-nodes)
+;      (format T "current-depth:~a~%" current-depth)
       
-	(if child-nodes
-	    (if write-name
-		(cons write-name child-nodes)
-		(if (< 1 (length child-nodes))
-		    child-nodes
-		    (car child-nodes)))
-	    write-name)
+	write-tree
       ))
-
 
 ;;; A grammar specification for the DNA language
 (defparameter Creature-DNA-1
   (process-grammar-definition
-   '((?Start -> ?Expression)
+   '((?Start -> ((?Expression)))
      (?Expression ->
-      (;function defintion and application 
-       (lambda ?Symbol ?Expression =>  (lambda (type . lambda)))
-       (gamma  (?Expression (type . lambda))
-	       ?Expression => gamma)
-					;list
-       (cons ?Expression ?Expression => (cons (type . list)))
-       (cdr (?Expression (type . 'list)) => cdr)
-       (car (?Expression (type . 'list)) => car)
+      (;function defintion and application
+       
+       (lambda ?Symbol ?Expression
+	       =>
+	       (lambda ?Symbol ?Expression))
+       
+       (gamma ?Expression ?Expression
+	       =>
+	       (gamma ?Expression ?Expression))
+	;list
+       (cons ?Expression ?Expression
+	     =>
+	     (gamma (gamma cons ?Expression) ?Expression))
+       
+       (cdr ?Expression
+	    =>
+	    (gamma cdr ?Expression))
+       
+       (car ?Expression
+	    =>
+	    (gamma car ?Expression))
 
        
-					;boolean 
-       (or ?Expression ?Expression => or)
-       (if ?Expression ?Expression ?Expression => if)
-       (not ?Expression => (not (type . 'boolean)))
+	;boolean 
+       (or ?Expression ?Expression
+	   =>
+	   (gamma (gamma or ?Expression) ?Expression))
        
-					;predicates
-       (equal? ?Expression ?Expression => (equal (type . boolean)))
-       (type? ?Expression ?Type =>  (eq (type . boolean)))
+       (if ?Expression ?Expression ?Expression
+	   =>
+	   (gamma (gamma (gamma if ?Expression) (lambda nil ?Expression)) (lambda nil ?Expression)))
+	   
+       (not ?Expression
+	    => 
+	    (gamma not ?Expression))
+       
+	;predicates
+       (equal? ?Expression ?Expression
+	    =>
+	    (gamma (gamma equal ?Expression) ?Expression))
+       
+       (type? ?Expression ?Type
+	      =>
+	      (gamma (gamma eq ?Expression) ?Type))
        
 ;Environment
 ;(move (?Expression (type . 'node) (return . )) => move)
@@ -141,13 +196,11 @@ TODO: This should probably actually make some sort of struct rather than redicul
 ;(look (?Expression (type . 'node)) => look)  
        
        (?Terminal)
-       
-       
        ))
      (?Type ->
       ((node => node)
        (function => function)
-      (list => list)
+       (list => list)
        (atom => atom)
        (number => number)
        ))
@@ -161,9 +214,15 @@ TODO: This should probably actually make some sort of struct rather than redicul
        (t => t)
        (*gened-sym* => *gened-sym*)
       ;Environment
-       (move => move)
-       (energy? => energy?)
-       (feed => feed)
+       (move
+	=>
+	(gamma move nil))
+       (energy?
+	=>
+	(gamma energy? nil))
+       (feed
+	=>
+	(gamma feed nil))
        
        (?Type)))
      )))
